0591e3015ad813d57b64a8773238e41508f41fcb
Alexandr Rybin <missclick79@gmail.com>
2021-08-02 22:30:15 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-let radius = 5;
-let height = 10;
-let volume = Math.PI * (radius * radius) * height;
-console.log("Объём цилиндра V = " + volume);
+var distanceKm = +prompt("Введите расстояние в километрах");
+var distanceFoot = +prompt("Введите расстояние в футах");
+// Функция высчитывает коэффициент перевода километров в футы.
+function converter() {
+    var metersInKm = 1000;
+    var footsInMeter = 0.305;
+    var result = Math.round(metersInKm / footsInMeter);
+    return result;
+}
+console.log(converter());
+if ((distanceKm * converter()) > distanceFoot) {
+    alert("Расстояние в км больше");
+} else if ((distanceKm * converter()) < distanceFoot) {
+    alert("Расстояние в футах больше");
+} else alert("Расстояния равны");
+
