7a33109d6b5fb2e47cf6196d2252ec98542dacc6
Alexandr Rybin <gelikopterfl@gmail.com>
2021-08-06 01:41:57 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,47 @@
+/* let browser = prompt("Введите название");
+if (browser === 'IE') {
+    alert("IE");
+} else if (browser === 'Chrome') {
+    alert("Chrome");
+}else if (browser === 'Firefox') {
+    alert("Firefox");
+}else if (browser === 'Safari') {
+    alert("Safari");
+}else if (browser === 'Opera') {
+    alert("Opera");
+} else alert("Такого названия нет"); */
+
+/* let a = +prompt("Введите число от 0 до 3");
+switch (a) {
+    case 0: alert("0");
+        break;
+    case 1: alert("1");
+        break;
+    case 2:
+    case 3:
+        alert("2,3");
+        break;
+    default: {
+        alert("пусто");
+    }
+} */
+
+/* var userInfo = {
+    firstName: 'Aleksey',
+    MIDDLEName: '12312',
+    lastName: 'setttt',
+}
+
+function getfullName(first, middle, last, separate) {
+
+    return first + separate + middle + separate + last;
+}
+
+
+
+console.log('firstName--> ', userInfo.firstName);
+console.log('FIO--> ', getfullName(userInfo.firstName, userInfo.MIDDLEName, userInfo.lastName, '-'));
+console.log('FIO--> ', getfullName(userInfo.firstName, userInfo.MIDDLEName, userInfo.lastName, '_'));
+console.log('FIO--> ', getfullName(userInfo.firstName, userInfo.MIDDLEName, userInfo.lastName, '/'));
+ */
+
