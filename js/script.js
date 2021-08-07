767001574d215d8496fddce5c73a5c122440979b
Alexandr Rybin <gelikopterfl@gmail.com>
2021-08-07 20:20:33 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
-var questOne = +prompt("Сколько лет?");
-var questTwo = confirm("Куришь?");
-if (questOne < 18 && questTwo === false) alert("Так держать!");
-else if (questOne < 18 && questTwo === true) alert("Маме раскажу");
-else if (questOne > 18 && questTwo === false) alert("Молодец, и не надо");
-else alert("Ну и зря");
+//1способ
+
+function isLannisterSoldier(colorOfArmor, shield) {
+    if ((colorOfArmor === "red" && shield === null) || (shield === "lion")) {
+        return console.log("true");
+    } else return console.log("false");
+}
+isLannisterSoldier("red", null);
+isLannisterSoldier("red", "lion");
+isLannisterSoldier("red", "dog");
+isLannisterSoldier("blue", null);
+isLannisterSoldier("green", "lion");
+
+//2способ
+
+function isLannisterSoldier() {
+    let colorOfArmor = prompt("Какой цвет доспехов?");
+    let shield = confirm("Щит есть?");
+    let imageOnShield;
+    switch (shield) {
+        case true: imageOnShield = prompt("Что изображено на щите?");
+            break;
+        case false: break;
+    }
+    if ((colorOfArmor === "red" && shield === false) ||
+        (imageOnShield === "lion") ||
+        (colorOfArmor === "red" && imageOnShield === "lion")) {
+        return alert("It's Lannister!");
+    } else return alert("It's Not Lannister!");
+}
+isLannisterSoldier();
+
+
+
