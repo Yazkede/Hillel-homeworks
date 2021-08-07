7c903be068492dd4b167c368d363e30772c01dbd
Alexandr Rybin <gelikopterfl@gmail.com>
2021-08-07 18:31:26 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,47 +1,6 @@
-/* let browser = prompt("Введите название");
-if (browser === 'IE') {
-    alert("IE");
-} else if (browser === 'Chrome') {
-    alert("Chrome");
-}else if (browser === 'Firefox') {
-    alert("Firefox");
-}else if (browser === 'Safari') {
-    alert("Safari");
-}else if (browser === 'Opera') {
-    alert("Opera");
-} else alert("Такого названия нет"); */
-
-/* let a = +prompt("Введите число от 0 до 3");
-switch (a) {
-    case 0: alert("0");
-        break;
-    case 1: alert("1");
-        break;
-    case 2:
-    case 3:
-        alert("2,3");
-        break;
-    default: {
-        alert("пусто");
-    }
-} */
-
-/* var userInfo = {
-    firstName: 'Aleksey',
-    MIDDLEName: '12312',
-    lastName: 'setttt',
-}
-
-function getfullName(first, middle, last, separate) {
-
-    return first + separate + middle + separate + last;
-}
-
-
-
-console.log('firstName--> ', userInfo.firstName);
-console.log('FIO--> ', getfullName(userInfo.firstName, userInfo.MIDDLEName, userInfo.lastName, '-'));
-console.log('FIO--> ', getfullName(userInfo.firstName, userInfo.MIDDLEName, userInfo.lastName, '_'));
-console.log('FIO--> ', getfullName(userInfo.firstName, userInfo.MIDDLEName, userInfo.lastName, '/'));
- */
-
+var questOne = +prompt("Сколько лет?");
+var questTwo = confirm("Куришь?");
+if (questOne < 18 && questTwo === false) alert("Так держать!");
+else if (questOne < 18 && questTwo === true) alert("Маме раскажу");
+else if (questOne > 18 && questTwo === false) alert("Молодец, и не надо");
+else alert("Ну и зря");
