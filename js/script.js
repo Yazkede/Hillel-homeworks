c2c6c3c418c6972069ff706f42d5d6e2bd2a7c5f
Alexandr Rybin <gelikopterfl@gmail.com>
2021-08-29 19:12:53 +0300

    111

--- js/script.js
+++ js/script.js
@@ -1,54 +1,16 @@
-/* Функция должна найти и вернуть максимальный элемент в массиве.
- * В случае пустого массива вернуть: -1
-    *
- * Реализовать функцию двумя способами: через циклы и через рекурсию
-    *
- * @param { [] } input
-    * @returns { number }
- */
-
 //циклы===================================================================================
 
-function max(input) {
-    let maxItem = 0;
-    for (let i = 0; i < input.length; i++) {
-        if (input.length == 1) {
-            maxItem = input[i];
-        } else if (input[i] > input[i + 1] && input[i] > maxItem) {
-            maxItem = input[i];
-        } else if (input[i] > input[i + 1] && input[i] < maxItem) {
-            break;
-        } else if (input[i] < input[i + 1] && input[i + 1] > maxItem) {
-            maxItem = input[i + 1];
-        } else if (input[i] < input[i + 1] && input[i + 1] < maxItem) {
-            break;
+function max(arr) {
+    let max = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
         }
     }
-    if (input.length == 0) {
-        maxItem--;
-    } return maxItem;
-}
-console.assert(max([8]) === 8, 'one element test, must return 8');
-console.assert(max([8, 17]) === 17, '2 elements test, must return 17');
-console.assert(max([1, 8, 37, 5, 17]) === 37, '5 elements test, must return 37');
-console.assert(max([]) === -1, 'empty array test, must return -1');
-
-//пытался сделать рекурсию, но получилась не рекурсия=====================================
-
-function max(input) {
-    let maxItem = 0;
-    input.sort((a, b) => {
-        if (a > b && a > maxItem) {
-            maxItem = a;
-        } else if (b > maxItem) {
-            maxItem = b;
-        }
-    });
-    if (input.length == 0) {
+    if (arr.length == 0) {
         return -1;
-    } else if (input.length == 1) {
-        return input[0];
-    } else return maxItem;
+    }
+    return max;
 }
 console.assert(max([8]) === 8, 'one element test, must return 8');
 console.assert(max([8, 17]) === 17, '2 elements test, must return 17');
@@ -57,20 +19,21 @@ console.assert(max([]) === -1, 'empty array test, must return -1');
 
 //рекурсия================================================================================
 
-const max = (input) => {
-    let maxItem = 0;
-    for (let i = 0; i < input.length; i++) {
-        if (Array.isArray(input[i])) {
-            input[i] = max(input);
-        } else if (input[i] > maxItem) {
-            maxItem = input[i];
-        }
+function max(arr) {
+    if (arr.length == 1) {
+        return arr[0];
     }
-    if (input.length == 0) {
+    if (arr.length == 0) {
         return -1;
     }
-    return maxItem;
-};
+    let maxOne = arr[0];
+    arr.shift();
+    let maxTwo = max(arr);
+    if (maxOne > maxTwo) {
+        return maxOne;
+    }
+    return maxTwo;
+}
 console.assert(max([8]) === 8, 'one element test, must return 8');
 console.assert(max([8, 17]) === 17, '2 elements test, must return 17');
 console.assert(max([1, 8, 37, 5, 17]) === 37, '5 elements test, must return 37');
